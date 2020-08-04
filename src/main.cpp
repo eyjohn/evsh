d1338a88caea3cf1dc076834f976999ae5ea61d7
Evgeny Yakimov <john.yakimov@gmail.com>
2020-08-04 21:57:30 +0100

    Working basic shell
    - Minor CMake adjustements/cleanup
    - `evsh` now works as a basic shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+#include "commandline.h"
+#include "exec.h"
+#include <csignal>
 #include <iostream>
+
+using namespace evsh;
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    std::signal(SIGINT, SIG_IGN);
+    for (;;) {
+        std::string line;
+        std::cout << "$ ";
+        if (!std::getline(std::cin, line)) {
+            break;
+        }
+        const auto commandLine = parse(line);
+        auto rc = exec(commandLine);
+    }
     return 0;
 }
