2289cf8991ae24e75f01c0286f73f867738b77d2
Evgeny Yakimov <john.yakimov@gmail.com>
2020-08-17 21:59:33 +0100

    fail

--- test/test_exec.u.t.cpp
+++ test/test_exec.u.t.cpp
@@ -17,5 +17,5 @@ TEST(TestExec, InvalidCommand)
 TEST(TestExec, ValidCommand)
 {
     EXPECT_THAT(exec({"sh", "-c", "exit 42"}), Eq(42));
-    EXPECT_TRUE(false);
+    EXPECT_TRUE(false) << "yup fail";
 }
